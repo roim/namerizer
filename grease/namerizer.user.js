fc19038f1e6f5499c89e2f23496c1cad1061d59f
Rodrigo Roim <roim.rodrigo@gmail.com>
2013-05-04 03:23:29 -0300

    Caching calls to the nickname API (persistent and in-memory).

--- grease/namerizer.user.js
+++ grease/namerizer.user.js
@@ -8,6 +8,18 @@
 // @grant GM_setValue
 // ==/UserScript==
 
+//
+// Constants
+//
+
+var cacheKey = "namerizerCache";
+var failCode = "fail";
+var baseServiceAddress = "http://namerizer.herokuapp.com";
+
+//
+// Function definitions
+//
+
 // These should be provided by GM, but they're not supported in Chrome :(
 // Soo, if these are not defined, we implement them in the HTLM5 way. 
 //
@@ -23,20 +35,44 @@ if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString
   };
 }
 
-// Cache (Stub)
+var nicknameList;
+function updateCache (jsonContent) {
+	// Memory
+	updateMemoryCache(jsonContent);
+	// Persistent
+	GM_setValue(cacheKey, jsonContent);
+}
+
+function updateMemoryCache (jsonContent) {
+	nicknameList = JSON.parse(jsonContent);
+}
+
+// TODO, lag, set the user ID
+var userId = 42;
+
+//
+// Initialization
 //
 
-GM_setValue( "namerizerCache", JSON.stringify("rOimS2Lag") );
-
-alert( JSON.parse(GM_getValue("namerizerCache", "{}")) );
-
-// Retrieve preferred nicknames from our APIs
+// Set-up the cache
 //
+
+// First, we load the persistent cache
+var persistentJson = GM_getValue(cacheKey, failCode);
+
+if (persistentJson != failCode) {
+	updateMemoryCache(persistentJson);
+}
 
 GM_xmlhttpRequest({
   method: "GET",
-  url: "http://www.xboxfusion.com/",
+  // If the persistent request failed, there's nothing to do before the API returns
+  synchronous: (persistentJson == failCode),
+  url: baseServiceAddress + "/fetch/" + userId,
   onload: function(response) {
-    alert(response.responseText);
+    // TODO, roim, maybe onload means status == 200?
+    if (response.status != 200) return;
+
+    updateCache(response.responseText);
   }
 });
