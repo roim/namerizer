e313ee95bc57159d8ac72d9616dd22622554b468
Rodrigo Roim <roim.rodrigo@gmail.com>
2013-05-03 23:24:54 -0300

    Initial commit for the greasemonkey script. Includes persistent caching funcionality.

--- grease/namerizer.user.js
+++ grease/namerizer.user.js
@@ -1,8 +1,29 @@
 // ==UserScript==
-// @name Webmonkey's Hello World
+// @name Namerizer Grasemonkey Script
 // @namespace http://www.webmonkey.com
-// @description A basic example of Greasemonkey that causes an alert at each page load.
+// @description Namerizer renames the names of your named friends.
 // @include *
+// @grant GM_getValue
+// @grant GM_setValue
 // ==/UserScript==
 
-alert('Monkey sez... "Hello World!"');
+// These should be provided by GM, but they're not supported in Chrome :(
+// Soo, if these are not defined, we implement them in the HTLM5 way. 
+if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported")>-1)) {
+  this.GM_getValue=function (key,def) {
+      return localStorage[key] || def;
+  };
+  this.GM_setValue=function (key,value) {
+      return localStorage[key]=value;
+  };
+  this.GM_deleteValue=function (key) {
+      return delete localStorage[key];
+  };
+}
+
+// Cache
+//
+
+GM_setValue( "namerizerCache", JSON.stringify("rOimS2Lag") );
+
+alert( JSON.parse(GM_getValue("namerizerCache"), "{}") );
