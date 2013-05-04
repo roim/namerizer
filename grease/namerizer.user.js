b520e3ec343637ff6249257a6a06c7fd6fc7c9f2
Rodrigo Roim <roim.rodrigo@gmail.com>
2013-05-04 00:18:05 -0300

    Sample http request to be used later when our APIs are ready.

--- grease/namerizer.user.js
+++ grease/namerizer.user.js
@@ -3,12 +3,14 @@
 // @namespace http://www.webmonkey.com
 // @description Namerizer renames the names of your named friends.
 // @include *
+// @grant GM_xmlhttpRequest
 // @grant GM_getValue
 // @grant GM_setValue
 // ==/UserScript==
 
 // These should be provided by GM, but they're not supported in Chrome :(
 // Soo, if these are not defined, we implement them in the HTLM5 way. 
+//
 if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported")>-1)) {
   this.GM_getValue=function (key,def) {
       return localStorage[key] || def;
@@ -21,9 +23,20 @@ if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString
   };
 }
 
-// Cache
+// Cache (Stub)
 //
 
 GM_setValue( "namerizerCache", JSON.stringify("rOimS2Lag") );
 
-alert( JSON.parse(GM_getValue("namerizerCache"), "{}") );
+alert( JSON.parse(GM_getValue("namerizerCache", "{}")) );
+
+// Retrieve preferred nicknames from our APIs
+//
+
+GM_xmlhttpRequest({
+  method: "GET",
+  url: "http://www.xboxfusion.com/",
+  onload: function(response) {
+    alert(response.responseText);
+  }
+});
