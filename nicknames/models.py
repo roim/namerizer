5b473fe3a433dd8b7909112518fdc358a9846682
Rodrigo Roim <roim.rodrigo@gmail.com>
2013-05-05 20:49:36 -0300

    Converting our backend to Go using Google Appengine. This commit includes the Hello World example from Appengine's tutorials.

--- nicknames/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Nickname(models.Model):
-    source = models.BigIntegerField()
-    target = models.BigIntegerField()
-    alias = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    username = models.CharField(max_length=200)
--- nicknames/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from nicknames import views
-
-urlpatterns = patterns('nicknames.views',
-    url(r'^(?P<src>\d+)/$', views.fetch, name='fetch'),
-    url(r'^create/(?P<src>\d+)/(?P<tgt>\d+)/(?P<nick>\w+)/(?P<usr>\w+)/(?P<usrname>\w+)', views.update, name='update')
-)
