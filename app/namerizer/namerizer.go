3616d0883ba47b657314376e52e1053743ab6a85
Rodrigo Roim <roim.rodrigo@gmail.com>
2013-05-06 05:54:57 -0300

    Implementing in Go the 3 APIs the django app currently supports:     . Get Nicknames     . Add Nickname     . Get Suggestions

--- app/namerizer/namerizer.go
+++ app/namerizer/namerizer.go
@@ -7,7 +7,9 @@ import (
 	"fmt"
 	"net/http"
 	"regexp"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 type Nickname struct {
@@ -18,47 +20,53 @@ type Nickname struct {
 	Username string
 }
 
-func init() {
-	http.HandleFunc("/getNicknamesForUser/", getNicknamesForUser)
-	http.HandleFunc("/addNewNickname", addNewNickname)
+type Pair struct {
+	Key   string
+	Value int
 }
 
-func getNicknamesForUser(w http.ResponseWriter, r *http.Request) {
-	// Validate request
+type PairList []Pair
 
-	urlRegex, _ := regexp.Compile("^/getNicknamesForUser/")
-	id, err := strconv.ParseInt(urlRegex.ReplaceAllString(r.URL.String(), ""), 10, 64)
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value } // Since we want in descending order
 
-	if err != nil {
-		http.Error(w, "400: Invalid id", 400)
-		return
+func sortMapKeysByValue(m map[string]int, qtd int) []string {
+	p := make(PairList, len(m))
+	i := 0
+	for k, v := range m {
+		p[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(p)
+
+	var min int
+	if qtd < len(m) {
+		min = qtd
+	} else {
+		min = len(m)
 	}
 
-	// Process request
-
-	c := appengine.NewContext(r)
-
-	q := datastore.NewQuery("Nickname").Filter("AuthorId =", id)
-
-	results := make([]Nickname, 0, 3000)
-
-	if _, err := q.GetAll(c, &results); err != nil {
-		http.Error(w, "500: Failure querying the databse", 500)
-		return
+	keys := make([]string, min)
+	for j := 0; j < min; j++ {
+		keys[j] = p[j].Key
 	}
 
-	b, err := json.Marshal(results)
+	return keys
+}
 
-	if err != nil {
-		http.Error(w, "500: Failure encoding result array to json", 500)
-		return
-	}
-
-	fmt.Fprint(w, string(b))
+func init() {
+	http.HandleFunc("/addNewNickname", addNewNickname)
+	http.HandleFunc("/getNicknamesForUser/", getNicknamesForUser)
+	http.HandleFunc("/getSuggestions/", getSuggestions)
 }
 
 func addNewNickname(w http.ResponseWriter, r *http.Request) {
 	// Validate request
+	if r.Method != "POST" {
+		http.Error(w, "403: Method forbidden", 400)
+		return
+	}
 
 	requestBody := make([]byte, r.ContentLength)
 
@@ -98,4 +106,102 @@ func addNewNickname(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+}
+
+func getNicknamesForUser(w http.ResponseWriter, r *http.Request) {
+	// Validate request
+	if r.Method != "GET" {
+		http.Error(w, "403: Method forbidden", 400)
+		return
+	}
+
+	urlRegex, _ := regexp.Compile("^/getNicknamesForUser/")
+	id, err := strconv.ParseInt(urlRegex.ReplaceAllString(r.URL.String(), ""), 10, 64)
+
+	if err != nil {
+		http.Error(w, "400: Invalid id", 400)
+		return
+	}
+
+	// Process request
+
+	c := appengine.NewContext(r)
+
+	q := datastore.NewQuery("Nickname").Filter("AuthorId =", id)
+
+	results := make([]Nickname, 0, 3000)
+
+	if _, err := q.GetAll(c, &results); err != nil {
+		http.Error(w, "500: Failure querying the databse", 500)
+		return
+	}
+
+	b, err := json.Marshal(results)
+
+	if err != nil {
+		http.Error(w, "500: Failure encoding result array to json", 500)
+		return
+	}
+
+	fmt.Fprint(w, string(b))
+}
+
+func getSuggestions(w http.ResponseWriter, r *http.Request) {
+	// Validate request
+	if r.Method != "GET" {
+		http.Error(w, "403: Method forbidden", 400)
+		return
+	}
+
+	urlRegex, _ := regexp.Compile("^/getSuggestions/")
+	urlWithoutApiName := urlRegex.ReplaceAllString(r.URL.String(), "")
+
+	idQtd := strings.Split(urlWithoutApiName, "?qtd=")
+
+	if len(idQtd) != 2 {
+		http.Error(w, "400: Invalid request", 400)
+		return
+	}
+
+	id, err := strconv.ParseInt(idQtd[0], 10, 64)
+	if err != nil {
+		http.Error(w, "400: Invalid id", 400)
+		return
+	}
+
+	qtd, err := strconv.ParseInt(idQtd[1], 10, 64)
+	if err != nil {
+		http.Error(w, "400: Invalid qtd", 400)
+		return
+	}
+
+	// Process request
+
+	c := appengine.NewContext(r)
+
+	q := datastore.NewQuery("Nickname").Filter("TargetId =", id).Limit(3000)
+
+	nicknames := make([]Nickname, 0, 3000)
+
+	if _, err := q.GetAll(c, &nicknames); err != nil {
+		http.Error(w, "500: Failure querying the databse", 500)
+		return
+	}
+
+	var nickFrequency map[string]int = make(map[string]int)
+
+	for i := 0; i < len(nicknames); i++ {
+		nickFrequency[nicknames[i].Alias]++
+	}
+
+	results := sortMapKeysByValue(nickFrequency, int(qtd))
+
+	b, err := json.Marshal(results)
+
+	if err != nil {
+		http.Error(w, "500: Failure encoding result array to json", 500)
+		return
+	}
+
+	fmt.Fprint(w, string(b))
 }
