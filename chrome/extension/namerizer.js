7d6760a7498de83016e8a733457c8d8f4049bdb5
Victor Elias <victorgelias@gmail.com>
2013-05-07 11:05:45 -0300

    Using .forEach instead of normal fors (may be faster). Also fixed the bug when the chat names changed, now we change it back :)

--- chrome/extension/namerizer.js
+++ chrome/extension/namerizer.js
@@ -1,4 +1,3 @@
-
 function findElementsDirectlyContainingText(ancestor, text) {
 	var childrenContaining = $(ancestor).find(":contains(" + text + ")");
 	if (childrenContaining.length === 0) {
@@ -13,34 +12,33 @@ function findElementsDirectlyContainingText(ancestor, text) {
 
 function configureNodeAnimation(parentNode, whereToReplace, name, alias) {
 	$(parentNode).on('mouseenter', function(event) {
-		if ($(parentNode).find(event.fromElement).size()) return;
-		for (var i in whereToReplace) {
-			node = whereToReplace[i];
+		if ($.contains(parentNode, event.fromElement)) return;
+		$(parentNode).attr('namerized', 'false');
+		whereToReplace.forEach(function(node) {
 			fadeReplaceInText(node, alias, name);
-		}
+		});
 	});
 	$(parentNode).on('mouseleave', function(event) {
-		if ($(parentNode).find(event.toElement).size()) return;
-		for (var i in whereToReplace) {
-			node = whereToReplace[i];
+		if ($.contains(parentNode, event.toElement)) return;
+		$(parentNode).attr('namerized', 'true');
+		whereToReplace.forEach(function(node) {
 			fadeReplaceInText(node, name, alias);
-		}
+		});
 	});
 }
 
 function replaceName(parentNode, target) {
-	if(!target || $(parentNode).attr('namerized') === 'true') {
+	if(!target || ($(parentNode).attr('namerized') === 'true' && $(parentNode).html().indexOf(target.name) === -1)) {
 		return false;
 	}
 	var whereToReplace = findElementsDirectlyContainingText(parentNode, target.name);
 	if (whereToReplace.length == 0)
 		return false;
-	for (var i in whereToReplace) {
-		node = whereToReplace[i];
+	whereToReplace.forEach(function(node) {
 		if ($(node).text().indexOf(target.name) !== -1) {
 			$(node).text($(node).text().replace(target.name, target.alias));
 		}
-	}
+	});
 	$(parentNode).attr('namerized', 'true');
 	configureNodeAnimation(parentNode, whereToReplace, target.name, target.alias);
 	return false;
@@ -65,14 +63,22 @@ function switchNames(links) {
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations) {
-	for (var i in mutations) {
-		if (mutations[i].type == 'childList' && mutations[i].addedNodes) {
-			var addedNodes = mutations[i].addedNodes;
+	mutations.forEach(function(mutation) {
+		if (mutation.type == 'childList' && mutation.addedNodes) {
+			var addedNodes = mutation.addedNodes;
 			for (var j = 0; j < addedNodes.length; j++) {
 				switchNames($(addedNodes[j]).find('a'));
+				if ($(addedNodes[j]).prop('tagName') === 'A') {
+					switchNames([addedNodes[j]]);
+				}
+			}
+			// this checks the node that has been modified, we will only try to switch names on it if its already namerized or we don't care :)
+			// also, when the user hovers we change the text as well, but we change the namerized attribute to false so we won't try to swith it back either
+			if ($(mutation.target).prop('tagName') === 'A' && $(mutation.target).attr('namerized') === 'true') {
+				switchNames([mutation.target]);
 			}
 		}
-	}
+	});
 });
 
 observer.observe(document, {
